54ba54c5b6277d9e9d30091bde1f3d305a1692ea
b1mind <brent@1mindsolutions.com>
2022-07-20 15:16:28 -0600

    📷 trying to find away to get good thumb nails for feed list

--- src/routes/feed/[rss].js
+++ src/routes/feed/[rss].js
@@ -3,16 +3,40 @@ import Parser from 'rss-parser'
 
 let parser = new Parser()
 
+// const isValidImageUrl = (url) => {
+// 	if ((!url && typeof url !== 'string') || url.length === 0) return
+
+// 	const allowedExtensions = /\.(jpe?g|png|gif)$/i
+// 	if (url.match(allowedExtensions)) return true
+// }
+
 export async function GET({ url, params }) {
-	let xmlUrl = url.searchParams.has('xml') ? url.searchParams.get('xml') : false
+	const xmlUrl = url.searchParams.has('xml')
+		? url.searchParams.get('xml')
+		: false
 
 	// txml wants a string of xml only ...
 	// let feed = await txml.toContentString(txml.parse(xmlUrl))
-	let feed = await parser.parseURL(xmlUrl)
+	//
+	const feed = await parser.parseURL(xmlUrl)
 	let items = []
 
 	feed.items.forEach((item) => {
 		let newItem = { title: item.title, link: item.link }
+
+		// // would love to grab an img each item or least the list
+		// const imgElement = /src\s*=\s*"(.+?)"/g
+		// if (!item.content.includes('img')) return
+		// let imgs = item.content.match(imgElement)
+		// imgs = imgs.filter((img) => {
+		// 	if (!isValidImageUrl(img)) {
+		// 		return img
+		// 	}
+		// })
+		// console.log(imgs[0])
+		// // let img = imgs[0].replace('src=', '').replaceAll('"', '')
+		// // newItem = { ...newItem, img: img }
+
 		items = [...items, newItem]
 	})
 
--- src/routes/feed/[rss].svelte
+++ src/routes/feed/[rss].svelte
@@ -6,10 +6,12 @@
 <h1>{title}</h1>
 
 <ul>
-	{#each items as { title, link }}
+	{#each items as { title, link, img }}
 		<a href={link}>
 			<li>{title}</li>
 		</a>
+
+		<!-- <img src={img} alt={title} /> -->
 	{/each}
 </ul>
 
