d15613df8d78697daba558423a348edf9aba4ea2
b1mind <brent@1mindsolutions.com>
2022-11-05 21:52:59 -0600

    🧙‍♂️ working read and logout

--- src/routes/friends/+page.svelte
+++ src/routes/friends/+page.svelte
@@ -1,42 +1,30 @@
 <script>
-	throw new Error("@migration task: Add data prop (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292707)");
-
-	import { session } from '$app/stores'
 	import { enhance } from '$lib/utils/form.js'
 
-	export let friends = []
+	export let data
 </script>
 
 <main>
-	{#if $session.user}
-		<h1>Friends</h1>
-		<ul>
-			{#each friends as friend}
-				<li>
-					<a href={friend.webId}> {friend.nick} </a>
-					<ul>
-						{#each friend.rssList as list}
-							<li>
-								<a href="/feed/{list.name}?xml={list.href}"> {list.name} </a>
-								<form
-									data-loading="false"
-									action="/feed"
-									method="POST"
-									use:enhance
-								>
-									<input type="hidden" name="feed" bind:value={list.name} />
-									<input type="hidden" name="url" bind:value={list.href} />
-									<button type="submit">➕test</button>
-								</form>
-							</li>
-						{/each}
-					</ul>
-				</li>
-			{/each}
-		</ul>
-	{:else}
-		<p>nothing to display <a href="/login">login</a></p>
-	{/if}
+	<h1>Friends</h1>
+	<ul>
+		{#each data.friends as friend}
+			<li>
+				<a href={friend.webId}> {friend.nick} </a>
+				<ul>
+					{#each friend.rssList as list}
+						<li>
+							<a href="/feed/{list.name}?xml={list.href}"> {list.name} </a>
+							<form data-loading="false" action="/feed?/add" method="POST">
+								<input type="hidden" name="feed" bind:value={list.name} />
+								<input type="hidden" name="url" bind:value={list.href} />
+								<button type="submit">➕test</button>
+							</form>
+						</li>
+					{/each}
+				</ul>
+			</li>
+		{/each}
+	</ul>
 </main>
 
 <style lang="scss">
