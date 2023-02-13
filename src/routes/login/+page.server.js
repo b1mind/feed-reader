e4a177af479a70cfbb4555fa2ab003d2d5336d31
b1mind <brent@1mindsolutions.com>
2023-02-12 23:58:22 -0700

    🔒 login setup with form actions to enter provider

--- src/routes/login/+page.server.js
+++ src/routes/login/+page.server.js
@@ -1,48 +1,40 @@
 import { invalid, redirect } from '@sveltejs/kit'
 import { Session } from '@inrupt/solid-client-authn-node'
 
-export async function load({ locals, url, cookies }) {
-	console.log('login session')
-	let redirectUrl = ''
-	let solidSession = new Session()
+export async function load({ locals, url, cookies }) {}
 
-	cookies.set('session', solidSession.info.sessionId, {
-		path: '/',
-		httpOnly: true,
-		// sameSite: 'strict',
-		// secure: process.env.NODE_ENV === 'production',
-		secure: true,
-		maxAge: 60 * 60 * 24,
-	})
+export const actions = {
+	sendIt: async ({ url, cookies, request }) => {
+		const formData = await request.formData()
 
-	const handleSolidRedirect = (url) => {
-		return (redirectUrl = url)
-	}
+		console.log('login session')
+		let redirectUrl = ''
+		let solidSession = new Session()
 
-	await solidSession.login({
-		// After login, the Solid Identity Provider will send the user back to the following
-		// URL, with the data necessary to complete the authentication process
-		// appended as query parameters:
-		redirectUrl: `${url.origin}/authorize`,
-		// redirectUrl: `${url.href}`,
-		// Set to the user's Solid Identity Provider; e.g., "https://login.inrupt.com"
-		oidcIssuer: 'https://inrupt.net/',
-		// oidcIssuer: 'https://solidcommunity.net/',
-		// oidcIssuer: 'https://broker.pod.inrupt.com',
-		// Pick an application name that will be shown when asked
-		// to approve the application's access to the requested data.
-		clientName: 'Feed Reader',
-		handleRedirect: handleSolidRedirect,
-	})
+		cookies.set('session', solidSession.info.sessionId, {
+			path: '/',
+			httpOnly: true,
+			secure: true,
+			maxAge: 60 * 60 * 24,
+		})
 
-	//can we set a cookie header with this redirect?
-	throw redirect(302, redirectUrl)
+		const handleSolidRedirect = (url) => {
+			return (redirectUrl = url)
+		}
+
+		await solidSession.login({
+			redirectUrl: `${url.origin}/authorize`,
+			// Set to the user's Solid Identity Provider; e.g., "https://inrupt.net/"
+			oidcIssuer: formData.get('provider') || 'https://inrupt.net/',
+			clientName: 'Feed Reader',
+			handleRedirect: handleSolidRedirect,
+		})
+
+		//should we set a header with this redirect?
+		throw redirect(302, redirectUrl)
+	},
+
+	bawls: async () => {
+		//testing ground
+	},
 }
-
-// export const actions = {
-// 	async default({ locals, cookies }) {
-// 		if (!locals.session) throw redirect(302, '/')
-
-// 		throw redirect(302, '/')
-// 	},
-// }
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,8 @@
+<br />
+<form method="POST" action="/login?/sendIt">
+	<label for="provider"
+		>Pod provider
+		<input name="provider" type="text" value="https://inrupt.net/" />
+	</label>
+	<button>login</button>
+</form>
