9d78cc49354f08ccdac53e2c336d3506699b886d
b1mind <brent@1mindsolutions.com>
2024-01-29 18:08:54 -0700

    🔧 session logout fix

--- src/routes/auth/logout/+server.js
+++ src/routes/auth/logout/+server.js
@@ -11,7 +11,7 @@ export async function POST({ locals, cookies }) {
 	)
 	console.timeEnd('logout getSesh')
 
-	if (session) session.logout()
+	if (session) await session.logout()
 	await locals.session.destroy()
 	//should I even keep this cookie or wipe when we created sesh
 
