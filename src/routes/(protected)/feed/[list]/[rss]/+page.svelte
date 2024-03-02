94dcc24fb79444814c9d689fd89cf86d67a1c5e6
b1mind <brent@1mindsolutions.com>
2024-03-01 17:09:39 -0700

    feature: have seen to rss feeds | dirty needs abstraction + combined with list feeds

--- src/routes/(protected)/feed/[list]/[rss]/+page.svelte
+++ src/routes/(protected)/feed/[list]/[rss]/+page.svelte
@@ -1,5 +1,28 @@
 <script>
+	import { actionLocalStorage } from '$lib/utils'
+	import { afterUpdate } from 'svelte'
+
 	export let data
+
+	let lastSeen = ''
+
+	afterUpdate(() => {
+		let hasSeenJson = localStorage.getItem('hasSeen')
+		if (!hasSeenJson) return
+		lastSeen = JSON.parse(hasSeenJson)
+	})
+
+	function saveSeen(e, title) {
+		e.target.closest('li').classList.add('seen')
+		actionLocalStorage('save', 'hasSeen', title, false)
+	}
+
+	function hideSeen() {
+		const allSeen = document.querySelectorAll('.seen')
+		for (const post of allSeen) {
+			post.classList.toggle('hidden')
+		}
+	}
 </script>
 
 <hgroup>
@@ -7,10 +30,11 @@
 	<p>{data.rss?.description || 'no description'}</p>
 </hgroup>
 
+<button type="button" on:click={hideSeen}> hideSeen</button>
 <ul>
 	{#each data.rss.items as { title, link, snippet, published, ogImage }}
-		<li>
-			<a href={link}>
+		<li class:seen={lastSeen.includes(title)}>
+			<a href={link} on:click={(e) => saveSeen(e, title)}>
 				<!-- <a href="/feed/read/?link={link}"> -->
 				{title}
 			</a>
@@ -30,8 +54,20 @@
 	}
 
 	ul {
-		margin: 0;
 		padding: 0;
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+		gap: 10px;
+		list-style: none;
+	}
+
+	.seen {
+		color: red;
+	}
+
+	li {
+		padding: 0.5rem;
+		background: var(--clr-primary-bg-alt);
 	}
 
 	time {
