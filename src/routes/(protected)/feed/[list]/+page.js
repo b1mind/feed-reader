d935b7d6b0e79db15252d5009c2a66d5ec09ef6b
b1mind <brent@1mindsolutions.com>
2024-03-03 03:09:00 -0700

    fix: working sort

--- src/routes/(protected)/feed/[list]/+page.js
+++ src/routes/(protected)/feed/[list]/+page.js
@@ -1,5 +1,5 @@
-export async function load({ fetch, params }) {
-	const res = await fetch(`/api/feed/${params.list}`)
+export async function load({ fetch, params, url }) {
+	const res = await fetch(`/api/feed/${params.list}${url.search}`)
 
 	return {
 		feedStream: res.json(),
