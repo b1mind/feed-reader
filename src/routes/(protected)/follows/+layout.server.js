5d7331b5ac3c5da45b888973742b5b78430e4014
b1mind <brent@1mindsolutions.com>
2024-03-03 23:36:22 -0700

    test: no streamming friends list

--- src/routes/(protected)/follows/+layout.server.js
+++ src/routes/(protected)/follows/+layout.server.js
@@ -64,7 +64,7 @@ export async function load({ locals }) {
 			}
 		})
 
-		const friends = Promise.all(friendPromises)
+		const friends = await Promise.all(friendPromises)
 
 		return { friends }
 	} catch (error) {
