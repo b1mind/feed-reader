7c6874b1de69d19a6f75d32b177b757b39053749
b1mind <brent@1mindsolutions.com>
2024-03-09 21:58:26 -0700

    test: fix for svelte aliases

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import adapter from '@sveltejs/adapter-node'
 
@@ -5,6 +6,16 @@ import adapter from '@sveltejs/adapter-node'
 const config = {
 	kit: {
 		adapter: adapter(),
+	},
+	vite: {
+		resolve: {
+			alias: {
+				// these are the aliases and paths to them
+				'@components': path.resolve('./src/lib/components'),
+				$lib: path.resolve('./src/lib'),
+				'@utils': path.resolve('./src/lib/utils'),
+			},
+		},
 	},
 	preprocess: [vitePreprocess()],
 }
